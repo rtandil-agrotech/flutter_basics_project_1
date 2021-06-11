5456cbf9ae3a61a5fe63451ed1a9b90970b687af
Riko Tandil <riktandil@gmail.com>
2021-06-11 17:26:10 +0700

    Add Stateful Example

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,50 @@ void main() {
   runApp(App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
 
   @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  late int value;
+
+  @override
+  void initState() {
+    value = 0;
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    print(value);
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  value.toString(),
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    value = value + 1;
+                  });
+                },
+                child: Text('Add'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
