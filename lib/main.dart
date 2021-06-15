ef52c36d8e04680efacf083c2ddcd470d6a34c94
Riko Tandil <riktandil@gmail.com>
2021-06-15 08:44:12 +0700

    setup playground

--- lib/main.dart
+++ lib/main.dart
@@ -9,42 +9,6 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Step 1
-    // Create Material App Widget
-    // (Home)
-    return Container();
-  }
-}
-
-class Homepage extends StatefulWidget {
-  const Homepage({Key? key}) : super(key: key);
-
-  @override
-  _HomepageState createState() => _HomepageState();
-}
-
-class _HomepageState extends State<Homepage> {
-  // TODO: Step 2
-  // Initialize RandomDiceGenerator Class
-
-  // TODO: Step 4
-  // Create Scaffold which shows Dice Widget
-
-  // TODO: Step 5
-  // Implement setstate on button to re-set DiceWidget value
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
-class DiceWidget extends StatelessWidget {
-  const DiceWidget({Key? key}) : super(key: key);
-
-  // TODO: Step 3
-  // Create Dice Showing Widget which takes value as a parameter
-  @override
-  Widget build(BuildContext context) {
     return Container();
   }
 }
